3fd6307d315dd0d32216fb728f821adfc734f357
Tanner Lancaster <tanner@flatironschool.com>
2019-01-24 02:11:33 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -57,7 +57,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-		 myhashy =programmer_hash[:dennis_ritchie][:languages][0]
+		 rogrammer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
