0feec809001e9b28da29b54c846ae4ecabfbaf47
Tanner Lancaster <tanner@flatironschool.com>
2019-01-24 02:17:11 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -16,7 +16,7 @@ def hopper
         }
      }
 
-programmer_hash[:grace_hopper];
+		 return programmer_hash[:grace_hopper];
 end
 
 def alan_kay_is_known_for
@@ -38,7 +38,7 @@ def alan_kay_is_known_for
         }
      }
 
-		 programmer_hash[:alan_kay][:known_for];
+		 return programmer_hash[:alan_kay][:known_for];
 end
 
 def dennis_ritchies_language
@@ -57,7 +57,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-		 programmer_hash[:dennis_ritchie][:languages][0];
+		 return programmer_hash[:dennis_ritchie][:languages][0];
 end
 
 def adding_matz
@@ -83,8 +83,8 @@ def adding_matz
         }
      }
 
-programmer_hash[:yukihiro_matsumoto] = { :known_for => "Ruby", :languages => ["LISP", "C"] }
-programmer_hash
+		 programmer_hash[:yukihiro_matsumoto] = { :known_for => "Ruby", :languages => ["LISP", "C"] }
+		 return programmer_hash
 end
 
 def changing_alan
